741e2023254c9352785389d26cba0c0fe556a406
Lucky <vagyasri2011@gmail.com>
2021-04-21 14:13:53 +0200

    Complete Variable trial

---
+++ variable.rb
@@ -0,0 +1,13 @@
+#String
+my_var = "Lucky"
+puts my_var
+puts my_var.length
+second_var = my_var.reverse
+puts second_var.upcase
+puts second_var.downcase
+puts second_var.include?("k")
+
+#Array
+
+
+
